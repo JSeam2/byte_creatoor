1f67c40108f67623d9cee045ad7972726da361be
Alexander Camuto <alexander.camuto@st-hughs.ox.ac.uk>
2024-02-25 12:41:34 +0000

    Update write_loop.sh

--- write_loop.sh
+++ write_loop.sh
@@ -3,10 +3,10 @@
 
 # we try 1kb, 1mb, 1gb, 20gb 100gb,
 for i in 1024 1048576 1073741824 21474836480 107374182400; do
-    byte_creatoor -- -n $i -p $1 -b 8000
+    byte_creatoor -n $i -p $1 -b 8000
 done
 # 8kb 16kb 640kb 1mb 1GB
 for b in 8000 16000 640000 1000000 1000000000; do
-    byte_creatoor -- -n 21474836480 -p $1 -b $b
+    byte_creatoor -n 21474836480 -p $1 -b $b
 done
 
