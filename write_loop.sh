438fde1a2446b32019482433070b760e6fff0100
Jseam <hello.jseam@gmail.com>
2024-02-25 18:06:25 -0700

    fix: next loops (#1)

--- write_loop.sh
+++ write_loop.sh
@@ -1,12 +1,15 @@
 # bash script that writes a for loop over N bytes to a file
 # Usage: ./write_loop.sh <filename>
 
-# we try 1kb, 1mb, 1gb, 20gb 100gb,
-for i in 1024 1048576 1073741824 21474836480 107374182400; do
-    byte_creatoor -n $i -p $1 -b 8000
-done
-# 8kb 16kb 640kb 1mb 1GB
-for b in 8000 16000 640000 1000000 1000000000; do
-    byte_creatoor -n 21474836480 -p $1 -b $b
-done
+# file sizes
+sizes=(1024 1048576 1073741824 21474836480 107374182400)
+# block sizes
+blocks=(8000 16000 640000 1000000 1000000000)
 
+# Nested loop to iterate over sizes and blocks
+for i in "${sizes[@]}"; do
+    for b in "${blocks[@]}"; do
+        # Call byte_creatoor with current size and block
+        byte_creatoor -n $i -p $1 -b $b
+    done
+done
